f95c53932a700f5a2fa97f1f6b472e947084036e
Larry Xu <hilarryxu@gmail.com>
2022-06-13 17:23:41 +0800

    feat: add printf

---
+++ kernel/defs.h
@@ -0,0 +1,10 @@
+#ifndef TUTOS_DEFS_H_
+#define TUTOS_DEFS_H_
+
+#include "types.h"
+
+// printf.c
+void printf(char *fmt, ...);
+void panic(char *s);
+
+#endif  // TUTOS_DEFS_H_
---
+++ kernel/types.h
@@ -0,0 +1,21 @@
+#ifndef TUTOS_TYPES_H_
+#define TUTOS_TYPES_H_
+
+typedef char s8;
+typedef unsigned char u8;
+typedef short s16;
+typedef unsigned short u16;
+typedef int s32;
+typedef unsigned int u32;
+
+typedef long s64;
+typedef unsigned long u64;
+
+typedef long intptr_t;
+typedef unsigned long uintptr_t;
+typedef unsigned int size_t;
+typedef int ssize_t;
+
+#define NULL ((void *)0)
+
+#endif  // TUTOS_TYPES_H_
